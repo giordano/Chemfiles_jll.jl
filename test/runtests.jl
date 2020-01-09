6d16760ba4916db5f52664c12a147a4cad5e5f33
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-01-09 23:41:04 +0000

    Do not throw an error

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ const libchemfiles_splitpath = ["bin", "libchemfiles.dll"]
 const libchemfiles_path = normpath(joinpath(artifact_dir, libchemfiles_splitpath...))
 
 @info "Going to dlopen the library..."
-dlopen(libchemfiles_path)
+@show dlopen(libchemfiles_path, throw_error = false)
 @info "...done!"
 
 # using Chemfiles_jll
