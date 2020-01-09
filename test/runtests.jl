d1c8903e82bef3890311f25e93570e780ef6b7fe
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-01-09 23:54:34 +0000

    Try my build

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,16 @@ using Libdl, Pkg, Pkg.Artifacts
 @show dllist()
 
 const artifact_dir = abspath(artifact"Chemfiles")
-
 const libchemfiles_splitpath = ["bin", "libchemfiles.dll"]
 const libchemfiles_path = normpath(joinpath(artifact_dir, libchemfiles_splitpath...))
 
-@info "Going to dlopen the library..."
+if Sys.WORD_SIZE == 64
+    @info "Going to dlopen my library..."
+    @show dlopen("libchemfiles.dll", throw_error = false)
+    @info "...done!"
+end
+
+@info "Going to dlopen official library..."
 @show dlopen(libchemfiles_path, throw_error = false)
 @info "...done!"
 
