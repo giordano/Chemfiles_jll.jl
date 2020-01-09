4c7532c9e7a79136d32c2a09b4653d5750252073
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-01-09 23:37:05 +0000

    Manually dlopen the library

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,16 @@
-using Libdl
+using Libdl, Pkg, Pkg.Artifacts
 
 @show dllist()
 
-using Chemfiles_jll
+const artifact_dir = abspath(artifact"Chemfiles")
+
+const libchemfiles_splitpath = ["bin", "libchemfiles.dll"]
+const libchemfiles_path = normpath(joinpath(artifact_dir, libchemfiles_splitpath...))
+
+@info "Going to dlopen the library..."
+dlopen(libchemfiles_path)
+@info "...done!"
+
+# using Chemfiles_jll
 
 @show dllist()
